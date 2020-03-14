94e26e8aa46a9f3337530d0fa182275bd52091a3
christian <christianpelayo0824@gmail.com>
2020-03-14 21:28:25 +0800

    - Fix the function for fetching data from the source. - Christian 14 Mar 2020

--- src/main/java/io/project/virustracker/utility/CommonUtility.java
+++ src/main/java/io/project/virustracker/utility/CommonUtility.java
@@ -1,23 +1,14 @@
 package io.project.virustracker.utility;
 
-import io.project.virustracker.utility.common.DateUtil;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-
 public class CommonUtility {
 
     public static class CoronaUtil {
         public static String CORONA_VIRUS_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/[date].csv?raw=True";
-        public static String CSV_HEADER_PROVINCE_AND_STATE = "Province/State";
-        public static String CSV_HEADER_COUNTRY_AND_REGION = "Country/Region";
-        public static String CSV_HEADER_LAST_UPDATE = "Last Update";
-        public static String CSV_HEADER_CONFIRMED = "Confirmed";
-        public static String CSV_HEADER_DEATHS = "Deaths";
-        public static String CSV_HEADER_RECOVERED = "Recovered";
+        public static int CSV_HEADER_PROVINCE_AND_STATE = 0;
+        public static int CSV_HEADER_COUNTRY_AND_REGION = 1;
+        public static int CSV_HEADER_LAST_UPDATE = 2;
+        public static int CSV_HEADER_CONFIRMED = 3;
+        public static int CSV_HEADER_DEATHS = 4;
+        public static int CSV_HEADER_RECOVERED = 5;
     }
 }
